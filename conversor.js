8c22a68541e861e62205b173ecb8cbd4103a30ea
SamuelPRG <samuel.rojas@genstudents.org>
2024-09-23 00:33:49 -0300

    mejoras

--- conversor.js
+++ conversor.js
@@ -1,9 +1,9 @@
 function conversorDeUnidad() {
-    let gradosCelsius = parseInt(document.getElementById("gradosCelsius").value);
-    let gradosFahrenheit = (gradosCelsius * 9/5) + 32;
-    let gradosKelvin = gradosCelsius + 273.15;
+    const gradosCelsius = parseFloat(document.getElementById("gradosCelsius").value);
+    const gradosFahrenheit = (gradosCelsius * 9/5) + 32;
+    const gradosKelvin = gradosCelsius + 273.15;
 
-    let resultado = `<br>Grados Fahrenheit: ${gradosFahrenheit}°F<br>Grados Kelvin: ${gradosKelvin}K`;
+    const resultado = `<br>Grados Fahrenheit: ${gradosFahrenheit.toFixed(2)}°F<br>Grados Kelvin: ${gradosKelvin.toFixed(2)}K`;
 
 
     
